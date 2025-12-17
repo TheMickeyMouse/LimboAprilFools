6d097448164ff066b55d4923862c128476ae11aa
Mickey <secondMickey1108@gmail.com>
2025-12-17 22:45:51 +0800

    fixed library stuff and made keys spin + z axis

--- LimboApp.cpp
+++ LimboApp.cpp
@@ -1,13 +1,19 @@
 #include "LimboApp.h"
 
-#include "glp.h"
-
-LimboApp::LimboApp() {
-    texKeyHigh    = Graphics::Texture2D::LoadPNG("../keyhigh.png");
-    texKeyMain    = Graphics::Texture2D::LoadPNG("../keymain.png");
-    texKeyShadow  = Graphics::Texture2D::LoadPNG("../keyshadow.png");
+LimboApp::LimboApp() : gdevice(Graphics::GraphicsDevice::Initialize({ (int)WIDTH, (int)HEIGHT }, { .transparent = true })) {
+    // const Graphics::TextureLoadParams params = { .pixelated = true };
+    texKeyHigh    = Graphics::Texture2D::LoadPNG("../keyhighfix.png");
+    texKeyMain    = Graphics::Texture2D::LoadPNG("../keymainfix.png");
+    texKeyShadow  = Graphics::Texture2D::LoadPNG("../keyshadowfix.png");
     texKeyOutline = Graphics::Texture2D::LoadPNG("../keyoutline.png");
-    colorPalette = Graphics::Image::LoadPNG("../colorpalette.png");
+    Graphics::Image colorSrc = Graphics::Image::LoadPNG("../colorpalette.png");
+
+    for (int i = 0; i < 8; ++i) {
+        for (int tone = 0; tone < 3; ++tone) {
+            // image is flipped
+            colorPalette[i][tone] = (Math::fColor)colorSrc[{ tone, 7 - i }];
+        }
+    }
 
     Graphics::Render::UseBlendFunc(Graphics::BlendFactor::ONE, Graphics::BlendFactor::INVERT_SRC_ALPHA);
 }
@@ -16,9 +22,12 @@ bool LimboApp::Run() {
     gdevice.Begin();
     canvas.BeginFrame();
 
-    canvas.Fill(Math::fColor::White());
-    canvas.DrawRect({ { WIDTH * 0.05, HEIGHT * 0.15 }, { WIDTH * 0.95, HEIGHT * 0.85 } });
+    // canvas.Fill(Math::fColor::White());
+    // canvas.DrawRect({ { WIDTH * 0.05, HEIGHT * 0.15 }, { WIDTH * 0.95, HEIGHT * 0.85 } });
     // canvas.DrawText("Hello, World!", 60, { 560, 740 }, { .rect = { 800, 400 } });
+
+    SetSpinningKeys();
+
     DrawKeys();
 
     // jailbreak safety measure
@@ -32,11 +41,56 @@ bool LimboApp::Run() {
     return true;
 }
 
+void LimboApp::DrawKey(int index, float scale) {
+    const Math::fv2 screenPos = Project(keys[index].position);
+    const float size = scale * Z_CENTER / keys[index].position.z;
+    canvas.DrawTextureW(texKeyMain,    screenPos, size, true, { .tint = colorPalette[index][0] });
+    canvas.DrawTextureW(texKeyHigh,    screenPos, size, true, { .tint = colorPalette[index][1] });
+    canvas.DrawTextureW(texKeyShadow,  screenPos, size, true, { .tint = colorPalette[index][2] });
+    canvas.DrawTextureW(texKeyOutline, screenPos, size);
+}
+
 void LimboApp::DrawKeys() {
-    for (int i = 0; i < 8; i++) {
-        canvas.DrawTextureW(texKeyMain,    keys[i].position, 200, true, { .tint = (Math::fColor)colorPalette[{ 0, 7 - i }] });
-        canvas.DrawTextureW(texKeyHigh,    keys[i].position, 200, true, { .tint = (Math::fColor)colorPalette[{ 1, 7 - i }] });
-        canvas.DrawTextureW(texKeyShadow,  keys[i].position, 200, true, { .tint = (Math::fColor)colorPalette[{ 2, 7 - i }] });
-        canvas.DrawTextureW(texKeyOutline, keys[i].position, 200);
+    for (int i = 0; i < 8; i++) { // back keys
+        if (keys[i].position.z < Z_CENTER) continue;
+        DrawKey(i);
     }
+
+    canvas.Stroke(Math::fColor::White());
+    canvas.DrawText("Limbo", 216, { WIDTH * 0.3, HEIGHT * 0.7 }, { .rect = { WIDTH * 0.4, HEIGHT * 0.4 } });
+
+    for (int i = 0; i < 8; i++) { // front keys
+        if (keys[i].position.z > Z_CENTER) continue;
+        DrawKey(i);
+    }
+}
+
+Math::fv2 LimboApp::Project(Math::fv3 position) {
+    const Math::fv2 center = { WIDTH / 2, HEIGHT / 2 };
+    return center + (position.As2D() - center) * (Z_CENTER / position.z);
+}
+
+void LimboApp::SetSpinningKeys() {
+    const float time = gdevice.GetIO().Time.currentTime;
+    // const float time = 0;
+
+    using namespace Quasi::Math;
+    const Radians AXIS_TILT = 85.0_deg;
+    const fv3 mainAxis = fv3::FromSpheric(1, Radians(time * 0.3f), AXIS_TILT);
+    const Rotor3D tilt = Rotor3D::RotateTo({ 0, 1, 0 }, mainAxis);
+
+    const Rotor2D turn = 45.0_deg;
+    Rotor2D curr = Radians(time * 0.75f);
+
+    const fv3 origin = { WIDTH * 0.5f, HEIGHT * 0.5f, Z_CENTER };
+    fv3 localX = tilt * fv3 { WIDTH * 0.3f, 0, 0 };
+    fv3 localZ = tilt * fv3 { 0, WIDTH * 0.1f, WIDTH * 0.1f };
+    localX.z = 0;
+    localZ.z /= WIDTH * 0.33f;
+    for (auto& key : keys) {
+        key.position = localX * curr.Cos() + localZ * curr.Sin() + origin;
+        curr += turn;
+    }
+
+    // canvas.DrawLine({ WIDTH * 0.5f, HEIGHT * 0.5f }, Project(mainAxis * fv3 { 300, 300, 10 } + origin));
 }
--- LimboApp.h
+++ LimboApp.h
@@ -5,31 +5,35 @@
 using namespace Quasi;
 
 struct LimboKey {
-    Math::fv2 position;
+    Math::fv3 position;
 };
 
 class LimboApp {
-    static constexpr float WIDTH = 1920, HEIGHT = 1080;
+    static constexpr float WIDTH = 1920, HEIGHT = 1080, Z_CENTER = 1.0f;
 
-    Graphics::GraphicsDevice gdevice = Graphics::GraphicsDevice::Initialize({ (int)WIDTH, (int)HEIGHT }, { .transparent = true });
+    Graphics::GraphicsDevice gdevice;
     Graphics::Canvas canvas { gdevice };
 
     LimboKey keys[8] = {
-        {{ WIDTH * 0.2f, HEIGHT * 0.65f }},
-        {{ WIDTH * 0.4f, HEIGHT * 0.65f }},
-        {{ WIDTH * 0.6f, HEIGHT * 0.65f }},
-        {{ WIDTH * 0.8f, HEIGHT * 0.65f }},
-        {{ WIDTH * 0.2f, HEIGHT * 0.35f }},
-        {{ WIDTH * 0.4f, HEIGHT * 0.35f }},
-        {{ WIDTH * 0.6f, HEIGHT * 0.35f }},
-        {{ WIDTH * 0.8f, HEIGHT * 0.35f }},
+        {{ WIDTH * 0.2f, HEIGHT * 0.65f, Z_CENTER }},
+        {{ WIDTH * 0.4f, HEIGHT * 0.65f, Z_CENTER }},
+        {{ WIDTH * 0.6f, HEIGHT * 0.65f, Z_CENTER }},
+        {{ WIDTH * 0.8f, HEIGHT * 0.65f, Z_CENTER }},
+        {{ WIDTH * 0.2f, HEIGHT * 0.35f, Z_CENTER }},
+        {{ WIDTH * 0.4f, HEIGHT * 0.35f, Z_CENTER }},
+        {{ WIDTH * 0.6f, HEIGHT * 0.35f, Z_CENTER }},
+        {{ WIDTH * 0.8f, HEIGHT * 0.35f, Z_CENTER }},
     };
 
     Graphics::Texture2D texKeyMain, texKeyHigh, texKeyShadow, texKeyOutline;
-    Graphics::Image colorPalette;
+    Math::fColor colorPalette[8][3];
 public:
     LimboApp();
 
     bool Run();
+    void DrawKey(int index, float scale = 200);
     void DrawKeys();
+    static Math::fv2 Project(Math::fv3 position);
+
+    void SetSpinningKeys();
 };
